df7bc35573682cfbced6bb6a40c0dae27feb5bf7
codercui <codercui@163.com>
2018-09-26 13:38:32 +0800

    添加构造方法

--- src/main/java/com/example/demo/entity/domain/User.java
+++ src/main/java/com/example/demo/entity/domain/User.java
@@ -4,7 +4,7 @@ public class User {
     private int id;
     private String name;
     private String description;
-
+    public User(){}
     public User(int id, String name, String description) {
         this.id = id;
         this.name = name;
